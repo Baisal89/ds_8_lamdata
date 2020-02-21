f11c6460042738e29dbfc612fa2236d511d74c7c
Baisal89 <b.ergeshev@gmail.com>
2020-02-21 15:08:02 -0500

    added some proof of concept utility files

---
+++ lamdata_baisal89/__init__.py
@@ -0,0 +1,13 @@
+"""
+lamdata - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+# sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+#The reson that ONES and ZEROS are capitalized because python convetion
+# global variable are all caps
